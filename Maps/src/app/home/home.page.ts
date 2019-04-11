52dc956fe29c7ce956b5e24f227a3924bd9ef8a2
FelipeHolanda01 <felipe.holanda@happycodeschool.com>
2019-04-11 17:55:34 -0300

    Repostando

---
+++ Maps/src/app/home/home.page.ts
@@ -0,0 +1,44 @@
+import{Component,ViewChild, ElementRef} from '@angular/core';
+import{NavController, Platform} from'@ionic/angular';
+
+declare var google: any;
+
+
+@Component({
+  selector : 'page-home',
+  templateUrl:'home.page.html'
+})
+export class HomePage{
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  start = 'chicago, il';
+  end = 'chicago, il';
+  directionsService = new google.maps.directionsService;
+  directionsDisplay = new google.maps.directionsRenderer;
+
+  constructor(
+    public navCtrl:NavController,
+    private platform: Platform){
+      platform.ready().then(()=>{
+        this.initMap();
+      })
+    }
+
+    initMap(){
+      var happycode = {
+        lat: -23.625183,
+        lng: -46.737448
+      };
+      this.map = new google.maps.Map(this.mapElement.nativeElement,{
+        zoom: 16,
+        center: happycode,
+        disableDefaultUI: true
+      });
+      var marker = new google.maps.Marker({
+        position: happycode,
+        map:this.map,
+        title: 'My position!'
+      });
+    }
+  }
